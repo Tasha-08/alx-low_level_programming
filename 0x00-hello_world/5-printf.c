599d8769b2eb2c5d0a42361b67e0ffd1ab3b8607
Natasha <natashambedzi26@gmail.com>
2022-03-11 06:57:30 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  *Return: return 1 after program execution
  */
 int main(void)
-[
-printf("with proper grammar, but the outcome is a piece of art,\n");
-returnn (0);
-]
+{
+printf("with proper grammar, but the outrcome is a piece of art,\n");
+return (0);
+}
